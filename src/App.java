35417743ca5c9784d6c1a424a916cdf6e8af351f
javier <jriveradelgado3@gmail.com>
2025-11-19 18:40:07 +0100

    caballo

--- src/App.java
+++ src/App.java
@@ -2,4 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo");
     }
+    public static void caballo(){
+        System.out.println("El caballo se mueve en L");
+    }
 }
